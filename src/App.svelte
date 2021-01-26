27122e5749f9e5d46ef021f56702fdca3d095c6f
MZMohamed <zubair85@gmail.com>
2021-01-26 05:23:12 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<Nav />
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	<div class="px-4 py-8 border border-indigo-500 shadow-lg">This is a card</div>
+</main>
+
+<style></style>
